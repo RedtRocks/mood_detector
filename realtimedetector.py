7090d75463e66dca9134866137629cb9baea89bb
SanyamWadhwa07 <pointoftruth.in@gmail.com>
2024-08-17 16:07:36 +0530

    changed realtimedetector

--- realtimedetector.py
+++ realtimedetector.py
@@ -5,25 +5,19 @@ from tensorflow.keras.models import load_model
 
 
 model = load_model("facialemotionmodel.h5")
-
-
 haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(haar_file)
 
 
 def extract_features(image):
     feature = np.array(image)
-    feature = feature.reshape(1, 48, 48, 1)  # Reshape to match model's expected input
-    return feature / 255.0  # Normalize the input
-
+    feature = feature.reshape(1, 48, 48, 1)  
+    return feature / 255.0  
 
 webcam = cv2.VideoCapture(0)
 
-
 labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
-
 while True:
- 
     ret, im = webcam.read()
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
